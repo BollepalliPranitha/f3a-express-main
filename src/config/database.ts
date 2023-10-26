9efad7080d919293e07baf1e6f07acbb783a6b8c
Pranitha <pranithabollepalli20@gmail.com>
2023-10-26 15:05:10 -0500

    second commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,11 +2,16 @@
 
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('SoccerGame', 'root', 'pranitha', {
-  host: 'localhost', // Replace with your database host
+const sequelize = new Sequelize('sql9653877', 'sql9653877', 'UzcvWtMePv', {
+  host: 'sql9.freesqldatabase.com', // Replace with your database host
   dialect: 'mysql', // Replace with your database dialect (e.g., mysql, postgres, sqlite)
   logging: false, // Set to true to enable SQL query logging
 });
+// const sequelize = new Sequelize('SoccerGame', 'root', 'pranitha', {
+//   host: 'localhost', // Replace with your database host
+//   dialect: 'mysql', // Replace with your database dialect (e.g., mysql, postgres, sqlite)
+//   logging: false, // Set to true to enable SQL query logging
+// });
 (async () => {
   try {
     // Test the database connection
--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,20 @@ import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import { PORT } from '@/config';
-import AvailabilityData from '@/models/models'; // Import the AvailabilityData model
+import Player from '@/models/Player'; // Import the Player model
+import AvailabilityData from '@/models/models';
 
 export const app = express();
 
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
 
-// New route to fetch availability data based on teamName
 app.get('/api/availability/:teamName', async (req, res) => {
   const teamName = req.params.teamName;
   try {
-    // Fetch availability data based on teamName
     const availabilityData = await AvailabilityData.findAll({
-      where: { teamName }, // Filter by teamName
+      where: { teamName },
     });
 
     res.status(200).json(availabilityData);
@@ -25,13 +25,10 @@ app.get('/api/availability/:teamName', async (req, res) => {
   }
 });
 
-app.post('/update-availability', async (req, res) => {
-  const { playerName, matchDay, availability } = req.body;
-
+app.post('/api/availability/update-availability', async (req, res) => {
   try {
-    // Update availability in your database (replace with your actual database update code)
-    // Make sure to validate and sanitize user input
-    // Example using Sequelize:
+    const { playerName, matchDay, availability } = req.body;
+
     const [updatedRowsCount] = await AvailabilityData.update(
       { availability },
       {
@@ -42,20 +39,108 @@ app.post('/update-availability', async (req, res) => {
     if (updatedRowsCount === 1) {
       return res.status(200).json({ message: 'Availability updated successfully' });
     }
+
     return res.status(404).json({ error: 'Player or match day not found' });
   } catch (error) {
     console.error('Error updating availability:', error);
     return res.status(500).json({ error: 'Internal server error' });
   }
 });
+app.get('/api/players', async (req, res) => {
+  try {
+    // Fetch a list of players from your database or data source
+    const players = await Player.findAll(); // Adjust this based on your data model
 
+    res.status(200).json(players);
+  } catch (error) {
+    console.error('Error fetching players:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+app.post('/api/players', async (req, res) => {
+  try {
+    const { name, team } = req.body;
+
+    const player = await Player.create({
+      name,
+      team,
+    });
+
+    if (player) {
+      return res.status(200).json({ message: 'New player created successfully' });
+    }
+
+    return res.status(500).json({ error: 'Failed to create a new player' });
+  } catch (error) {
+    console.error('Error creating a new player:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+app.post('/api/availability/add-member', async (req, res) => {
+  try {
+    const { playerName, matchDay, availability, teamName } = req.body;
+
+    // Assuming you have an 'AvailabilityData' model for availability data
+    const newMember = await AvailabilityData.create({
+      playerName,
+      matchDay,
+      availability,
+      teamName,
+    });
+
+    if (newMember) {
+      return res.status(200).json({ message: 'New member added successfully' });
+    }
+
+    return res.status(500).json({ error: 'Failed to add a new member' });
+  } catch (error) {
+    console.error('Error adding a new member:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+app.delete('/api/players', async (req, res) => {
+  try {
+    const { playerName, team } = req.body;
+
+    // Delete the player from the 'Player' table
+    const deletedPlayer = await Player.destroy({
+      where: { name: playerName, team },
+    });
+
+    if (deletedPlayer) {
+      return res.status(200).json({ message: 'Player deleted successfully' });
+    }
+
+    return res.status(404).json({ error: 'Player not found' });
+  } catch (error) {
+    console.error('Error deleting player:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+app.delete('/api/availability/delete-member', async (req, res) => {
+  try {
+    const { playerName, matchDay, teamName } = req.body;
+
+    // Delete the member from the 'AvailabilityData' table
+    const deletedMember = await AvailabilityData.destroy({
+      where: { playerName, matchDay, teamName },
+    });
+
+    if (deletedMember) {
+      return res.status(200).json({ message: 'Member deleted successfully' });
+    }
+
+    return res.status(404).json({ error: 'Member not found' });
+  } catch (error) {
+    console.error('Error deleting member:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
 if (process.env.NODE_ENV === 'production') {
-  // If in production environment, use the PORT variable from config
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 } else {
-  // In the development environment, use a default port (e.g., 3000)
   app.listen(3000, () => {
     console.log('Server is running on port 3000');
   });
